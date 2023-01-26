df7832597a308ac83c1aaff82e7f951bb9b600ca
Hermann <e1971009@cmaisonneuve.qc.ca>
2023-01-25 23:30:17 -0500

    Initialisation front end
    - Vite
    - TailwindCSS
    - SocketIO Client

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import socket from "../socket";
+
+function App() {
+  socket.on("connect", () => {
+    console.log("connected");
+  });
+
+  useEffect(() => {
+    socket.emit("connection", "Hello from client");
+  }, []);
+
+  return (
+    <div className="App">
+      <a
+        href="#"
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </a>
+    </div>
+  );
+}
+
+export default App;
